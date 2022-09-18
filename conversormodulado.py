62a54ddebf66f9f430f40cfd055925fd887e4ee8
Natalia Gutierrez <109185909+Nathuc@users.noreply.github.com>
2022-09-18 03:27:15 -0500

    Add files via upload

---
+++ conversormodulado.py
@@ -0,0 +1,28 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("Cuatos pesos "+ tipo_pesos +" tienes?:")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + "dolares" )
+
+
+menu = """
+Bienveenido al conversor de monedas 💸
+Seleccione 
+1- pesos colombianos
+2 - pesos argentinos 
+3- pesos mexicanos
+Elija una opción:"""
+
+opcion = int(input(menu))
+
+if opcion == 1:
+  conversor("Colombianos", 3875)
+elif opcion == 2:
+    conversor("Argentinos", 65)
+elif opcion ==3:
+    conversor("Mexicanos", 24)
+else:
+    print('ingrese una opcion valida')
+    
